4b8062ab372e33629466b99a1dc5cfe8c9643c29
kush.varade.19 <kush.varade.19@gmail.com>
2025-04-18 11:56:57 +0530

    Tentatively accept is converted to rb

---
+++ lib/coarnotify/patterns/tentatively_accept.rb
@@ -0,0 +1,43 @@
+module COARNotify
+    module Patterns
+      # Pattern to represent a Tentative Accept notification
+      # https://coar-notify.net/specification/1.0.0/tentative-accept/
+      class TentativelyAccept < NotifyPattern
+        include NestedPatternObjectMixin
+        include SummaryMixin
+  
+        # Tentative Accept type (ActivityStreams Tentative Accept type)
+        TYPE = ActivityStreamsTypes::TENTATIVE_ACCEPT
+  
+        # Validates the TentativelyAccept pattern with additional constraints:
+        # - inReplyTo property is required
+        # - inReplyTo value must match object.id value
+        #
+        # @return [Boolean] true if valid
+        # @raise [ValidationError] if validation fails
+        def validate
+          ve = ValidationError.new
+          begin
+            super
+          rescue ValidationError => superve
+            ve = superve
+          end
+  
+          # Validate inReplyTo presence
+          required_and_validate(ve, Properties::IN_REPLY_TO, in_reply_to)
+  
+          # Validate inReplyTo matches object.id
+          obj_id = object ? object.id : nil
+          if in_reply_to != obj_id
+            ve.add_error(
+              Properties::IN_REPLY_TO,
+              "Expected inReplyTo to match object.id. inReplyTo: #{in_reply_to}, object.id: #{obj_id}"
+            )
+          end
+  
+          raise ve if ve.has_errors?
+          true
+        end
+      end
+    end
+  end
